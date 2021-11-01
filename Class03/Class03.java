298b53427afb9ff2eaf10ade69ad01afc4839a55
109b29698 <91447419+109b29698@users.noreply.github.com>
2021-11-01 12:53:26 +0800

    Update Class03.java

--- Class03/Class03.java
+++ Class03/Class03.java
@@ -1,4 +1,5 @@
 package Class03;
+import java.util.Scanner;
 class Data{
 	private String name;
 	private test score;
@@ -12,16 +13,27 @@ class Data{
 			math=m;
 		}
 		double avg() {
-			return (english+math)/2;
+			return (english+math)/2.;
 		}
+	}
+	Data(String s,int i,int j){
+		name=s;
+		this.score = new test(i,j);
 	}
 	void show(){
 		System.out.println("name="+name);
-		System.out.println("english="+score.english);
-		System.out.println("math="+score.math);
-		System.out.println("avg="+score.avg());
+		System.out.println("english="+this.score.english);
+		System.out.println("math="+this.score.math);
+		System.out.println("avg="+this.score.avg());
 	}
 }
 public class Class03 {
-	
+	public static void main(String args[]) {
+		int i = 85,j = 92,k=77,l=56;
+		String s1="Annie",s2="Brian";
+		Data d1 = new Data(s1,i,j);
+		d1.show();
+		Data d2 = new Data(s2,k,l);
+		d2.show();
+	}
 }
